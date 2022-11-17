5dfe639a6479a3ffa97e325046f555fc066d093c
Alex Nojenko <93285568+informationvulture@users.noreply.github.com>
2022-11-17 01:34:59 +0000

    Add bug fix and improved function
    The bug that existed was possibly a problem with Selenium. As I currently can't find how to fix it, I've introduced bs4 as a current working solution.
    
    This commit also includes the creation of a text file with the jobs each time the script is run.

--- CAF_crawler/main.py
+++ CAF_crawler/main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+import bs4
+from datetime import datetime
 
 options = Options()
 options.headless = True
@@ -25,9 +27,25 @@ driver.find_element(By.XPATH, label).click()
 
 driver.find_element(By.XPATH, closer).click()
 
-CLASS_NAME = "class name"
-# Get the job titles
+file = driver.page_source
 
-job_titles = driver.find_elements(By.CLASS_NAME, "job-card-title")
-print([job.text for job in job_titles])
 driver.quit()
+
+
+soup = bs4.BeautifulSoup(file, "html.parser")
+
+job_titles = soup.find_all("h2", {"class": "job-card-title"})
+formated_job_titles = [job.text for job in job_titles]
+
+MAX_LEN = len(sorted(formated_job_titles, key=len)[-1])
+
+print(f"There are currently \
+{len(formated_job_titles)} in-demand job postings on the CAF website.\n")
+
+print("They are:")
+for c,v in enumerate(formated_job_titles):
+    print(f"{c+1}. {v.ljust(MAX_LEN)}")
+
+with open(f"{datetime.today().strftime('%Y-%m-%d')}outputs.txt", "w", encoding="utf-8") as f:
+    for c,v in enumerate(formated_job_titles):
+        f.write(f"{c+1} {v}{' ' * MAX_LEN}\n")
